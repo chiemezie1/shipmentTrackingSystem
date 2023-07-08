bcf9d6376ee8f769c4b7f4b1f200aa04480d18ff
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-08 18:34:34 +0100

    Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const fs = require('fs');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL, PRIVATE_KEY } = process.env;
+
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+   }
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
