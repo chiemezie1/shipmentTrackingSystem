6c7aba56d35d475105fc2f935ef6c915aa2e1d48
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-08 18:32:28 +0100

    dotenv added

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  
+  const shipmentTracking = await hre.ethers.getContractFactory("ShipmentTracking");
+  const ShipmentTracking = await shipmentTracking.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await ShipmentTracking.deployed(); // Use the deployed contract instance
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log("ShipmentTracking deployed to:", ShipmentTracking.address);
 
-  await lock.waitForDeployment();
+  const data = {
+    address: ShipmentTracking.address,
+    abi: JSON.parse(ShipmentTracking.interface.format('json'))
+  };
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  //This writes the ABI and address to the eShipmentTracking.json
+  fs.writeFileSync('./client/Contract/ShipmentTracking.json', JSON.stringify(data));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
