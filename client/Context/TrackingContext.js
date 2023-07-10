21d7942a03454299bdd797ff876e3cf1c0f5813f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-10 12:27:29 +0100

    TrackingProvider fucntion created

--- client/Context/TrackingContext.js
+++ client/Context/TrackingContext.js
@@ -1,3 +1,45 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Web3Modal } from "@web3modal/react";
-import { ethers } from "ethers";
+import { Signer, ethers } from "ethers";
+import ShipmentTracking from "../Contract/ShipmentTracking.json";
+
+const fatchCcontract = (SignerOrProvider) => {
+    new ethers.Contract(
+        ShipmentTracking.address,
+        ShipmentTracking.abi,
+        SignerOrProvider
+    );
+};
+
+export const TrackingContext = React.createContext();
+
+export const TrackingProvider = async ({ children }) => {
+    const DAppName = "shipment Tracking";
+    const [currentUser, setCurrentUser] = useState("");
+
+    const createShipment = async (items) => {
+        const { receiver, pickUpTime, distance, price } = items
+    }
+
+    try {
+
+        const web3modal = new web3modal();
+        const connection = await web3modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+
+        const contract = fatchCcontract(signer);
+        const createItem = contract.createShipment(
+            receiver,
+            new Date(pickUpTime).getTime(),
+            distance,
+            ethers.utils.parseUnits(price, 18),
+            {
+                value: ethers.utils.parseUnits(price, 18),
+            });
+            await createItem.wait();
+
+    }catch (error) {
+        console.log("something went wrong",error)
+    }
+}
