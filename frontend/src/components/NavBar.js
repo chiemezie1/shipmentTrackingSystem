12f7932b6639f3858fd6a6796d0dfdb4387d9b2f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-11 23:29:57 +0100

    update page navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -65,14 +65,13 @@ const NavBar = () => {
                         </p>) : (
                             <button onclick={() => {
                                 connectWallet();
-                            }} className="flex items-end">
-
+                            }} className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                                connect wallet
                             </button>
                         )}
                     </div>
                 </div>
             </div>
-            NavBar
         </nav>
     )
 };
