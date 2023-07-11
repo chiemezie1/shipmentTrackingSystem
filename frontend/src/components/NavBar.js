65690b18c06eda3d9f04ca9ccdaec08dc14ac8c5
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-11 23:36:38 +0100

    update

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -24,55 +24,66 @@ const NavBar = () => {
     }, []);
 
     return (
-        <nav 
-        className={`bg-white pb-5 md:text-sm ${
-           state ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0": " "
-        }`}
+        <nav
+          className={`bg-white pb-5 md:text-sm ${
+            state
+              ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0"
+              : ""
+          }`}
         >
-            <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
-                <div className="flex items-center justify-between py-5 md:block">
-                    <a href="javascript:void(0)">
-                        <img
-                        src={logoIsolated}
-                        width={120}
-                        height={50}
-                        alt="Logo"
-                        />
-                    </a>
-                    <div className="md:hidden">
-                        <button className="menu-btu text-gray-500 hover:text-gray-800"
-                            onclick ={() => setState(!state)}>
-                                {state ? <Nav1 /> : <home />}
-                        </button>
-                    </div>
-                </div>
-                <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${state ? 'block' : 'hidden'}`}> 
-                    <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
-                        {Navigation.map((item, idx) => {
-                            return(
-                          <li key={idx} className="text-gray-700 hover:text-gray-900">
-                            <a href={item.path} className="block">
-                                {item.titile}
-                            </a>
-                          </li>
-                            );
-                        } )};
-
-                    </ul>
-                    <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
-                        {currentUser ? (<p className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                            {currentUser.slice(0,5) + "..." + currentUser.slice(32) }
-                        </p>) : (
-                            <button onclick={() => {
-                                connectWallet();
-                            }} className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                                connect wallet
-                            </button>
-                        )}
-                    </div>
-                </div>
+          <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+            <div className="flex items-center justify-between py-5 md:block">
+              <a href="javascript:void(0)">
+                <img
+                  src={logoIsolated}
+                  width={120}
+                  height={50}
+                  alt="Logo"
+                />
+              </a>
+              <div className="md:hidden">
+                <button
+                  className="menu-btu text-gray-500 hover:text-gray-800"
+                  onClick={() => setState(!state)}
+                >
+                  {state ? <Nav1 /> : <home />}
+                </button>
+              </div>
             </div>
+            <div
+              className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
+                state ? "block" : "hidden"
+              }`}
+            >
+              <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+                {Navigation.map((item, idx) => (
+                  <li key={idx} className="text-gray-700 hover:text-gray-900">
+                    <a href={item.path} className="block">
+                      {item.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+                {currentUser ? (
+                  <p className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                    {currentUser.slice(0, 5) + "..." + currentUser.slice(32)}
+                  </p>
+                ) : (
+                  <button
+                    onClick={() => {
+                      connectWallet();
+                    }}
+                    className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
+                  >
+                    Connect Wallet
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
         </nav>
-    )
+      );
+      
 };
 export default NavBar; 
