8125664c2e1cdeb2f6c16d7d9a256f89aa981e35
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-12 18:35:53 +0100

    navbar updated

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,89 +1,88 @@
 import React, { useContext, useEffect, useState } from "react";
 import { TrackingContext } from "../Context/TrackingContext";
-import { Nav1, home, logoIsolated, logo} from "../components/index";
-
-
+import { Nav1, Nav, cancel, logo } from "../components/index";
 
 const NavBar = () => {
+  const [state, setState] = useState(false);
+  const { currentUser, connectWallet } = useContext(TrackingContext);
 
-    const [state, setState] = useState(false);
-    const { currentUser, connectWallet } = useContext(TrackingContext);
+  const Navigation = [
+    { title: "Home", path: "#" },
+    { title: "Services", path: "#" },
+    { title: "About us", path: "#" },
+    { title: "Contact us", path: "#" },
+  ];
 
-    const Navigation = [
-        {titile: "Home", path: "#"},
-        {titile: "Services", path: "#"},
-        {titile: "About us", path: "#"},
-        {titile: "Contact us", path: "#"},
-    ];
+  useEffect(() => {
+    document.onclick = (e) => {
+      const target = e.target;
+      if (!target.closest(".menu-btu")) setState(false);
+    };
+  }, []);
 
-    useEffect(() => {
-        document.onclick =(e) =>{
-            const target = e.target;
-            if (!target.closest(".menu-btu")) setState(false)
-        }
-    }, []);
+  return (
+    <nav
+      className={`bg-white pb-5 md:text-sm ${
+        state
+          ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0"
+          : ""
+      }`}
+    >
+      <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+        <div className="flex items-center justify-between py-5 md:block">
+          <a href="javascript:void(0)">
+            <img src={logo} width={70} height={20} alt="Logo" />
+          </a>
 
-    return (
-        <nav
-          className={`bg-white pb-5 md:text-sm ${
-            state
-              ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0"
-              : ""
+          <div className="md:hidden">
+            <button
+              className="menu-btu text-gray-500 hover:text-gray-800"
+              onClick={() => setState(!state)}
+            >
+              {state ? (
+                <img src={cancel} width={40} height={20} alt="Logo" className="bg-gray-800 rounded-lg" />
+              ) : (
+                <img src={Nav} width={40} height={20} alt="Logo" className="bg-gray-800 rounded-lg"/>
+              )}
+            </button>
+          </div>
+
+        </div>
+
+
+        <div
+          className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
+            state ? "block" : "hidden"
           }`}
         >
-          <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
-            <div className="flex items-center justify-between py-5 md:block">
-              <a href="javascript:void(0)">
-                <img
-                  src={logoIsolated}
-                  width={120}
-                  height={50}
-                  alt="Logo"
-                />
-              </a>
-              <div className="md:hidden">
-                <button
-                  className="menu-btu text-gray-500 hover:text-gray-800"
-                  onClick={() => setState(!state)}
-                >
-                  {state ? <Nav1 /> : <home />}
-                </button>
-              </div>
-            </div>
-            <div
-              className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
-                state ? "block" : "hidden"
-              }`}
-            >
-              <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
-                {Navigation.map((item, idx) => (
-                  <li key={idx} className="text-gray-700 hover:text-gray-900">
-                    <a href={item.path} className="block">
-                      {item.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-              <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
-                {currentUser ? (
-                  <p className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                    {currentUser.slice(0, 5) + "..." + currentUser.slice(32)}
-                  </p>
-                ) : (
-                  <button
-                    onClick={() => {
-                      connectWallet();
-                    }}
-                    className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
-                  >
-                    Connect Wallet
-                  </button>
-                )}
-              </div>
-            </div>
+          <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+            {Navigation.map((item, idx) => (
+              <li key={idx} className="text-gray-700 hover:text-gray-900">
+                <a href={item.path} className="block">
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+            {currentUser ? (
+              <p className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                {currentUser.slice(0, 5) + "..." + currentUser.slice(32)}
+              </p>
+            ) : (
+              <button
+                onClick={() => {
+                  connectWallet();
+                }}
+                className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
+              >
+                Connect Wallet
+              </button>
+            )}
           </div>
-        </nav>
-      );
-      
+        </div>
+      </div>
+    </nav>
+  );
 };
-export default NavBar; 
+export default NavBar;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -8,16 +8,14 @@ import Services from "./Services";
 import StartShipment from "./StartShipments";
 import Table from "./Table";
 
-import Nav1 from "./SVG/_cancel.svg";
-import cancel from"./SVG/cancel.svg";
-import home from "./SVG/home.svg";
+import Nav from "./SVG/navicon.svg";
+import Nav1 from "./SVG/nav_icon.svg";
 import check from "./SVG/check.svg";
 import location from "./SVG/location.svg";
 import payment from "./SVG/payment.svg";
 import rating from './SVG/rating.svg';
-import  Wallet  from "./SVG/wallet.svg";
-import  logo  from "./SVG/default.svg";
-import  logoIsolated  from "./SVG/isolated-layout.svg";
+import  logo  from "./SVG/logo.svg";
+import  cancel  from "./SVG/cancel.svg";
 
 
 
@@ -32,14 +30,12 @@ export{
     StartShipment,
     Table,
 
-    home,
+    Nav,
     Nav1,
     check,
     location,
     payment,
     rating,
-    Wallet,
     cancel,
-    logoIsolated,
     logo,
 }
