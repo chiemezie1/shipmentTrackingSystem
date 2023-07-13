077e9e962d7799d7f5cfcb7fe6efe65848fb6abe
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-13 23:21:23 +0100

    user profile page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const About = () => {
-    return(
-        <div>
-            About
-        </div>
+    return (
+
+      <div>hellonnd</div>
+
     )
 };
 export default About; 
