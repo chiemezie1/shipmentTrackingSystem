4fe64bfd337cc2275c4985ed9ebeab19119170f6
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-15 20:40:21 +0100

    getshhipmenr data

--- frontend/src/components/GetShipment.js
+++ frontend/src/components/GetShipment.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GetShipment = () => {
-    return(
+const GetShipment = ({ getShipment, setGetModel, getModel}) => {
+    const [index, setIndex] = useState(0);
+    const [singleShipment, setSingleShipment] = useState();
+
+    const getShipmentData = async () => {
+        const getData = await getShipment(index);
+        setSingleShipment(getData);
+    };
+
+    const convertTime = (time) => {
+        const newTime = new Date(time);
+        const formatTime = new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        }).format(newTime);
+        return formatTime;
+    }
+    return getModel ? (
         <div>
             GetShipment
         </div>
-    )
+    ):("");
 };
 export default GetShipment; 
