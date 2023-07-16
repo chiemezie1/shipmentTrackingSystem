84f741a65980c09505b010a887493ca86b05ae8c
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-16 04:42:58 +0100

    shipment complete page

--- frontend/src/components/CompleteShipment.js
+++ frontend/src/components/CompleteShipment.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CompleteShipment = () => {
-    return(
-        <div>
-            CompleteShipment
+const CompleteShipment = ({
+    completeModal,
+    setCompleteModal,
+    completeShipment,
+}) => {
+    const [completeShipData, setCompleteShipData] = useState({
+        receiver: "",
+        index: "",
+    })
+    const changeSatuts = async () => {
+       await CompleteShipment(completeShipData);
+    }
+    return completeModal ? (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="fixed inset-0 w-full bg-black opacity-40"
+            onClick={() => setCompleteModal(false)}></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+                <div className="relative mx-auto w-full max-w-lg p-4 bg-white rounded-md shadow-lg">
+                    <div className="flex justify-end">
+                        <button
+                        className="p-2 text-gray-400 hover:text-gray-100 rouneded-md"
+                        onClick={() => setCompleteModal(false)}
+                        >
+                            <svg
+                            className="w-5 h-5"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+                        <h4 className=" text-lg font-medium text-gray-800">
+                            Complete Shipment
+                        </h4>
+                        <p className="text-[15px] text-gray-500">
+                            Complete shipment with blockchain and Ethereum blockchain in fast Transparent and secure way 
+                        </p>
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <div className="relative mt-3">
+                                 <input 
+                            type="text"
+                            placeholder="receiver"
+                            onChange={(e) => setCompleteShipData({...CompleteShipment, receiver: e.target.value})}
+                            className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                            />
+                            </div>
+                            <div className="relative mt-3">
+                                <input 
+                                type="number"
+                                placeholder="Id"
+                                onChange={(e) => setCompleteShipData({...CompleteShipment, index: e.target.value})}
+                                className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                />
+                            </div>
+                            <button onClick={() => changeSatuts()} className="block mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2">
+                                Change Status 
+                            </button>   
+                        </form>
+                    </div>
+                </div>          
+            </div>
         </div>
-    )
+    ):("")
 };
 export default CompleteShipment; 
